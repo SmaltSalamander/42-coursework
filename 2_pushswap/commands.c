96ad333a98a2feb859bd2ef61cd6df666a77d6f5
SmaltSalamander <sandrahanrin@gmail.com>
2021-08-20 13:56:23 +0200

    Permission change for checker, working commands implemented

--- 2_pushswap/commands.c
+++ 2_pushswap/commands.c
@@ -19,14 +19,17 @@ void	ft_swapping(t_list *arr, t_list *wrkngstck)
 	int	initswapb;
 
 	initswapb = 0;
-	if (wrkngstck->content && ft_lstsize(wrkngstck) > 1)
+	if (wrkngstck)
 	{
-		if (*(int *) wrkngstck->content < *(int *) (wrkngstck->next)->content)
+		if (wrkngstck->content && ft_lstsize(wrkngstck) > 1)
 		{
-			temp = *(int *) wrkngstck->content;
-			*(int *) wrkngstck->content = *(int *) (wrkngstck->next)->content;
-			*(int *) (wrkngstck->next)->content = temp;
-			initswapb = 1;
+			if (*(int *) wrkngstck->content < *(int *) (wrkngstck->next)->content)
+			{
+				temp = *(int *) wrkngstck->content;
+				*(int *) wrkngstck->content = *(int *) (wrkngstck->next)->content;
+				*(int *) (wrkngstck->next)->content = temp;
+				initswapb = 1;
+			}
 		}
 	}
 	temp = *(int *) arr->content;
@@ -38,59 +41,84 @@ void	ft_swapping(t_list *arr, t_list *wrkngstck)
 		ft_putstr_fd("sa\n", 1);
 }
 
-void	ft_pushing_pb(t_list *arr, t_list **wrkngstck)
+void	ft_pushing_pb(t_list **arr, t_list **wrkngstck)
 {
-	ft_lstadd_front(wrkngstck, arr);
-	*arr = *arr->next;
+	t_list	*ele;
+
+	ele = *arr;
+	ft_lstadd_front(wrkngstck, ft_lstnew(ele->content));
+	*arr = (*arr)->next;
+	free(ele);
 	ft_putstr_fd("pb\n", 1);
 }
 
-void	ft_pushing_pa(t_list **arr, t_list *wrkngstck)
+void	ft_pushing_pa(t_list **arr, t_list **wrkngstck)
 {
-	ft_lstadd_front(arr, wrkngstck);
-	*wrkngstck = *wrkngstck->next;
+	t_list	*ele;
+
+	ele = *wrkngstck;
+	ft_lstadd_front(arr, ft_lstnew(ele->content));
+	*wrkngstck = (*wrkngstck)->next;
+	free(ele);
 	ft_putstr_fd("pa\n", 1);
 }
 
-// void	ft_rotating(int *arr, int **wrkngstck, int *wrkbegin, int *arrlen)
-// {
-// 	int	counter;
-// 	int	wkstcklen;
-// 	int temp;
+void	ft_rotating_rb(t_list **wrkngstck)
+{
+	t_list	*ele;
 
-// 	counter = 0;	 	
-// 	temp = **arr;
-// 	**arr = *(*arr + 1);
-// 	*(*arr + 1) = temp;
-// 	while (counter < arrlen)
-// 	{
-// 		*(arr + counter) = *(arr + counter + 1);
-// 		counter++;
-// 	}
-// 	ft_putstr_fd("ra\n", 1);
-// }
+	ele = *wrkngstck;
+	ft_lstadd_back(wrkngstck, ft_lstnew(ele->content));
+	*wrkngstck = (*wrkngstck)->next;
+	free(ele);
+	ft_putstr_fd("rb\n", 1);
+}
 
-void	issue_commands(t_list *arr, t_list	*wrkngstck, int *arrlen)
+void	ft_rotating_ra(t_list **arr)
+{
+	t_list	*ele;
+
+	ele = *arr;
+	ft_lstadd_back(arr, ft_lstnew(ele->content));
+	*arr = (*arr)->next;
+	free(ele);
+	ft_putstr_fd("ra\n", 1);
+}
+
+void	issue_commands(t_list **arr, t_list	**wrkngstck, int *arrlen)
 {
 	t_list	*check1;
 
-	check1 = arr->next;
-	if ((*(int *)(arr)->content) > *(int *) check1->content)
-		ft_swapping(arr, wrkngstck);
-	else if ((*(int *)(arr)->content) < *(int *) check1->content)
+	check1 = (*arr)->next;
+	if (check1 != 0x0)
 	{
-		if (*(int *) wrkngstck->content)
+		if ((*(int *)(*arr)->content) > *(int *) check1->content)
+			ft_swapping(*arr, *wrkngstck);
+		else if ((*(int *)(*arr)->content) < *(int *) check1->content)
 		{
-			ft_pushing_pa(&arr, wrkngstck);
+			if ((*wrkngstck))
+			{
+				ft_pushing_pa(arr, wrkngstck);
+				*arrlen = *arrlen + 1;
+				return ;
+			}
+			while (((*(int *)(*arr)->content) < *(int *) check1->content) && *arrlen > 2)
+			{
+				ft_pushing_pb(arr, wrkngstck);
+				*arrlen = *arrlen - 1;
+			}
+		}
+	}
+	else
+	{
+		if (*(int *) (*wrkngstck)->content)
+		{
+			ft_pushing_pa(arr, wrkngstck);
 			*arrlen = *arrlen + 1;
 			return ;
 		}
-		while (((*(int *)(arr)->content) < *(int *) check1->content) && *arrlen > 2)
-		{
-			ft_pushing_pb(arr, &wrkngstck);
-			*arrlen = *arrlen - 1;
-		}
 	}
+	
 }
 
 char	ft_sort(t_list **arr, int arrlen)
@@ -99,12 +127,12 @@ char	ft_sort(t_list **arr, int arrlen)
 	int	lencpy;
 
 	lencpy = arrlen;
-	workingstack = ft_lstnew(ft_calloc(1, 1));
-	while (ft_check_order(*arr, lencpy) || *(int *)workingstack->content)
+	workingstack = 0;
+	while (ft_check_order(*arr, lencpy) || lencpy != arrlen)
 	{
-		issue_commands(*arr, workingstack, &lencpy);
+		issue_commands(arr, &workingstack, &lencpy);
 	}
-	// if (workingstack)
-	// 	ft_lstclear(&workingstack, free);
+	if (workingstack)
+		ft_lstclear(&workingstack, free);
 	return (0);
 }
