d7c29f3ec9e7eb51c5c3b23d37b29e382c203d60
SmaltSalamander <sandrahanrin@gmail.com>
2021-09-17 16:49:53 +0200

    Works on Mac now

---
+++ 2_so_long/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbienias <sbienias@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 14:52:44 by sbienias          #+#    #+#             */
+/*   Updated: 2021/09/17 16:27:22 by sbienias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilbxMac/mlx.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
