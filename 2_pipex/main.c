ac467548676530a0ae19a12eadd9b5b93ce6360b
SmaltSalamander <sandrahanrin@gmail.com>
2021-10-24 13:18:36 +0200

    Pipex WiP

--- 2_pipex/main.c
+++ 2_pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: sbienias <sbienias@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 21:12:30 by sbienias          #+#    #+#             */
-/*   Updated: 2021/10/20 20:15:13 by sbienias         ###   ########.fr       */
+/*   Updated: 2021/10/23 19:27:30 by sbienias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 int main(int argc, char **argv)
 {
 	char	**str;
+	int		ends[2];
+	pid_t	processid;
+
 	if (argc < 5)
 		return (1);
 	str = ft_split(argv[2], ' ');
-	execve((const char *) argv[2], (char * const*) argv[1], \
-	(char * const*)*(str + 1));
+	execve((const char *) argv[2], (char const *) argv[1], \
+	(char const *)*(str + 1));
 	pipe();
 	fork();
 }
