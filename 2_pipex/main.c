f0d701c907abdd667484328e63eccf530628901a
SmaltSalamander <sandrahanrin@gmail.com>
2021-10-31 22:20:06 +0100

    Getting execve to work

--- 2_pipex/main.c
+++ 2_pipex/main.c
@@ -6,24 +6,77 @@
 /*   By: sbienias <sbienias@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 21:12:30 by sbienias          #+#    #+#             */
-/*   Updated: 2021/10/23 19:27:30 by sbienias         ###   ########.fr       */
+/*   Updated: 2021/10/31 22:18:56 by sbienias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include "pipex.h"
-// < file1 cmd1 | cmd2 > file2
-int main(int argc, char **argv)
+
+char	**find_path(char **envp)
 {
+	int		i;
+	char	**result;
+	char	*tmp;
+
+	i = 0;
+	while (ft_strncmp(envp[i], "PATH=", 4) != 0)
+		i++;
+	result = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (result[i])
+	{
+		tmp = ft_strjoin(result[i], "/");
+		free(result[i]);
+		result[i] = tmp;
+		i++;
+	}
+	return (result);
+}
+
+void	execute_command(char **str, char **envp)
+{
+	char	*cmd;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+	{
+		cmd = ft_strjoin(envp[i], *str);
+		ft_printf("%s\n", cmd);
+		execve((char const *) cmd, (char *const *)(str + 1), envp);
+		free(cmd);
+		i++;
+	}
+	ft_putstr_fd("failed all\n", 1);
+	return ;
+}
+
+// < file1 cmd1 | cmd2 > file2
+int	main(int argc, char **argv, char **envp)
+{
+	// int		fd1;
+	// int		fd2;
 	char	**str;
 	int		ends[2];
 	pid_t	processid;
 
-	if (argc < 5)
+	if (argc < 2)
 		return (1);
+	// fd1 = open(argv[1], O_RDONLY);
+	// fd2 = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	str = ft_split(argv[2], ' ');
-	execve((const char *) argv[2], (char const *) argv[1], \
-	(char const *)*(str + 1));
-	pipe();
-	fork();
+	envp = find_path(envp);
+	pipe(ends);
+	processid = fork();
+	if (processid < 0)
+	{
+		perror("Fork failed");
+		return (1);
+	}
+	if (processid == 0)
+		execute_command(str, envp);
+	else
+		ft_putstr_fd("\ntest\n", 1);
+	return (0);
 }
--- 2_pipex/pipex.h
+++ 2_pipex/pipex.h
@@ -6,13 +6,14 @@
 /*   By: sbienias <sbienias@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 21:14:54 by sbienias          #+#    #+#             */
-/*   Updated: 2021/10/20 18:53:30 by sbienias         ###   ########.fr       */
+/*   Updated: 2021/10/31 21:32:44 by sbienias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 # include "libft/libft.h"
-
+#include <stdio.h>
+#include <errno.h>
 
 #endif
