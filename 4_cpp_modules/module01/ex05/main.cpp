295969d3b2420faf91f47e1e066d627dcc8d4cbc
SmaltSalamander <sandrahanrin@gmail.com>
2022-03-25 16:46:49 +0100

    ex05 test

---
+++ 4_cpp_modules/module01/ex05/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbienias <sbienias@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 10:27:53 by sbienias          #+#    #+#             */
+/*   Updated: 2022/03/25 16:45:34 by sbienias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <string>
+using std::string;
+#include "Harl.hpp"
+
+int main()
+{
+	Harl	harl;
+	harl.complain("WARNING");
+}
