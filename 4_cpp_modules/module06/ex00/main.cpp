5a38f37e53b6cbffe83bfaed3e486185c0e94a3b
SmaltSalamander <sandrahanrin@gmail.com>
2022-04-28 21:35:29 +0200

    reading up about casting

---
+++ 4_cpp_modules/module06/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbienias <sbienias@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 10:27:53 by sbienias          #+#    #+#             */
+/*   Updated: 2022/04/28 21:34:11 by sbienias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::string		literal;
+
+	if (argc != 2)
+		return (0);
+	literal = argv[1];
+	if (literal.length() > 0)
+	{
+		unsigned char ch;
+		
+	}
+	return (0);
+}
