d9c2d430f0dafc3ea44bf281684f454e5408d5e9
SmaltSalamander <sandrahanrin@gmail.com>
2022-05-02 17:21:57 +0200

    ex00 work, need to implement pseudoliterals

--- 4_cpp_modules/module06/ex00/main.cpp
+++ 4_cpp_modules/module06/ex00/main.cpp
@@ -6,24 +6,92 @@
 /*   By: sbienias <sbienias@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 10:27:53 by sbienias          #+#    #+#             */
-/*   Updated: 2022/04/28 21:34:11 by sbienias         ###   ########.fr       */
+/*   Updated: 2022/05/02 16:59:37 by sbienias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
 #include <iostream>
+#include <climits>
+#include <limits>
+#include <iomanip>
+
+int	checkForPseudos(std::string str)
+{
+	double inf = std::numeric_limits<double>::infinity();
+	float inff = std::numeric_limits<float>::infinity();
+
+	if (!str.compare("-inff"))
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -" << inff << "f" << std::endl;
+		std::cout << "double: -" << inff << std::endl;
+	}
+	else if (!str.compare("inff") || !str.compare("+inff"))
+	{
+
+	}
+	else if (!str.compare("nanf"))
+	{
+
+	}
+	else if (!str.compare("-inf"))
+	{
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		std::cout << "float: -" << inf << "f" << std::endl;
+		std::cout << "double: -" << inf << std::endl;
+
+	}
+	else if (!str.compare("inf") || !str.compare("+inf"))
+	{
+
+	}
+	else if (!str.compare("nan"))
+	{
+
+	}
+	else
+		return (0);
+	return (1);
+}
 
 int main(int argc, char **argv)
 {
-	std::string		literal;
+	std::string				literal;
+	unsigned char			c;
+	long					intCheck;
+	float					f;
+	double					d;
 
 	if (argc != 2)
 		return (0);
-	literal = argv[1];
+	literal = (std::string)(char *) argv[1];
+	int status = checkForPseudos(literal);
+	if (status)
+		return (0);
 	if (literal.length() > 0)
 	{
-		unsigned char ch;
-		
+		if (literal.length() == 1 && !std::isdigit(literal.begin()[0]))
+			std::cout << "char: " << literal << std::endl;
+		else
+		{
+			c = (unsigned char)std::atoi((char *)argv[1]);
+			if (std::isprint(c))
+				std::cout << "char: " << c << std::endl;
+			else
+				std::cout << "char: Non displayable" << std::endl;
+		}
+		intCheck = std::atol((char *) argv[1]);
+		if (intCheck < INT_MIN || intCheck > INT_MAX)
+			std::cout << "int: impossible" << std::endl;
+		else
+			std::cout << "int: " << intCheck << std::endl;
+		f = (float) std::atof((char *) argv[1]);
+		std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
+		d = std::atof((char *) argv[1]);
+		std::cout << "double: " << d << std::endl;
 	}
 	return (0);
 }
